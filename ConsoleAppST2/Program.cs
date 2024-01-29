a7d763dd7278d7e3f5dd155bb7bc31b224f1bca1
Thao <dacthao>
2024-01-29 11:06:22 +0700

    disconnected

--- ConsoleAppST2/Program.cs
+++ ConsoleAppST2/Program.cs
@@ -121,6 +121,129 @@ namespace ConsoleAppST2
             }
         }
 
+        private static void HienThiDanhSachSinhVienNgatKetNoi(string connectionString)
+        {
+            string select_pro = "Select_tblSINHVIEN";
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                //conn.ConnectionString = connectionString;
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = select_pro;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        adapter.SelectCommand = cmd;
+                        using(DataTable dataTable = new DataTable())
+                        {
+                            adapter.Fill(dataTable);
+                            if(dataTable.Rows.Count > 0)
+                            {
+                                //Hien thi co dieu kien ra man hinh
+                                using(DataView dataView = new DataView(dataTable))
+                                {
+                                    dataView.RowFilter = "bGioiTinh = 'nu'";
+                                    dataView.Sort = "dNgaySinh ASC";
+                                    //Hien thi ra man hinh
+                                    foreach (DataRowView row in dataView)
+                                    {
+                                        Console.WriteLine("{0}\t{1}",
+                                                            row["sMaSV"],
+                                                            row["dNgaySinh"]);
+                                    }
+                                }
+
+
+                            }
+                            else
+                            {
+                                //khong co ban ghi nao ton tai
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private static bool XoaSinhVienNgatKetNoi(string connectionString, string masv)
+        {
+            string delete_pro = "Delete_tblSINHVIEN";
+            string select_pro = "Select_tblSINHVIEN";
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = select_pro;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using(SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        adapter.SelectCommand = cmd;
+                        using(DataTable dtSINHVIEN = new DataTable("tblSINHVIEN"))
+                        {
+                            adapter.Fill(dtSINHVIEN) ;
+                            using(DataSet dataSet = new DataSet())
+                            {
+                                //add tung dataTable va dataSet
+                                dataSet.Tables.Add(dtSINHVIEN);
+
+                                //tim ma sv can xoa
+                                dtSINHVIEN.PrimaryKey = new DataColumn[] { dtSINHVIEN.Columns["sMaSV"] };
+                                DataRow dataRow = dtSINHVIEN.Rows.Find(masv);
+                                dataRow.Delete();
+
+                                //dong bo du lieu len Server thong qua DeleteCommand
+                                cmd.CommandText = delete_pro;
+                                cmd.CommandType= CommandType.StoredProcedure;
+                                cmd.Parameters.Clear();
+                                cmd.Parameters.AddWithValue("@masv", masv);
+
+                                adapter.DeleteCommand = cmd;
+                                int i = adapter.Update(dataSet, "tblSINHVIEN");
+
+                                return i>0 ;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool ThemSinhVienNgatKetNoi(string connectionString, string masv, string ngaysinh)
+        {
+            string insert_proc = "Insert_tblSINHVIEN";
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using(SqlDataAdapter adapter = new SqlDataAdapter())
+                {
+                    adapter.SelectCommand = new SqlCommand("SELECT * FROM tblSINHVIEN", conn);
+                    DataTable dtSINHVIEN = new DataTable("tblSINHVIEN");
+                    adapter.Fill(dtSINHVIEN);
+
+                    //add tung dataTable vao dataSet
+                    DataSet dataSet = new DataSet();
+                    dataSet.Tables.Add(dtSINHVIEN);
+
+                    //them 1 dong moi vao dataTable
+                    DataRow row = dtSINHVIEN.NewRow();
+                    row["sMaSV"] = masv;
+                    row["dNgaySinh"] = ngaysinh;
+
+                    //dong bo du lieu vao CSDL
+                    using(SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = insert_proc;
+                        cmd.Parameters.AddWithValue("@masv", masv);
+                        //.....parameters
+
+                        adapter.InsertCommand = cmd;
+                        int i = adapter.Update(dataSet, "tblSINHVIEN");
+                        return i>0;
+                    }
+                }
+            }
+        }
         private static bool KiemTraKhoaChinh_SinhVien (string connection, string masv)
         {
             string checkID_proc = "KiemTraKhoaChinh_tblSINHVIEN";
